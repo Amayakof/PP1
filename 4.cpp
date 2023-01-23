05e30501651e40b14b71c938ee88da59c5627c3e
Zhaniya Koshkimbaeva <amayakof@Zhaniyas-MacBook-Air-2.local>
2023-01-23 08:48:29 +0600

    lab1/PP1

---
+++ 4.cpp
@@ -0,0 +1,10 @@
+//You are given integer number. Print out its square root value.
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    float a;
+    cin>>a;
+    cout<<sqrt(a);
+    return 0;
+}
